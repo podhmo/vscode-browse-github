0276929ff3a438b8d08878d6a9274d9a370febc1
podhmo <ababjam61+github@gmail.com>
2022-11-29 03:34:20 +0900

    fix: fix package.json and registered name

--- src/extension.ts
+++ src/extension.ts
@@ -12,7 +12,7 @@ export function activate (context: vscode.ExtensionContext): void {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  const disposable = vscode.commands.registerCommand('vscode-browse-github.helloWorld', () => {
+  const disposable = vscode.commands.registerCommand('browse-github.helloWorld', () => {
     // The code you place here will be executed every time your command is executed
     // Display a message box to the user
     vscode.window.showInformationMessage('Hello World from browse-github!')
