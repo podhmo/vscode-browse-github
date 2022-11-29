4a3d4beb5bc6cf5aaedc7551e10cbd30bc7c7268
podhmo <ababjam61+github@gmail.com>
2022-11-29 19:16:28 +0900

    xxx: investigation

--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,24 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode'
+import * as resolve from './resolve'
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate (context: vscode.ExtensionContext): void {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, your extension "vscode-browse-github" is now active!')
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
   const disposable = vscode.commands.registerCommand('browse-github.helloWorld', () => {
-    // The code you place here will be executed every time your command is executed
-    // Display a message box to the user
-    vscode.window.showInformationMessage('browse github').then(() => {
-      return vscode.env.openExternal(vscode.Uri.parse('https://github.com/podhmo/vscode-browse-github/blob/main/src/extension.ts#L18'))
+    // const githubURL = resolve.url({ file: 'src/extension.ts', lineno: 8 })
+    // console.log(githubURL)
+    // return vscode.env.openExternal(vscode.Uri.parse(githubURL))
+
+    const editor = vscode.window.activeTextEditor
+    if (editor === undefined) {
+      return
+    }
+
+    const fspath = editor.document.uri.fsPath
+    vscode.window.showInformationMessage(`browse github from ${fspath}`).then(() => {
     }).then(() => {}, (err: any) => { console.log(err); void vscode.window.showErrorMessage(err) })
   })
 
